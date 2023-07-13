aaf18479ead6d3349011544a171b63ce2df5d6e6
Jay Lim <jlimoman@gmail.com>
2023-07-13 10:30:11 -0600

    third commit

--- server/models.py
+++ server/models.py
@@ -1,3 +1,23 @@
+"""
+Remember to regularly run 
+flask db revision --autogenerate -m'<descriptive message>' 
+and flask db upgrade head 
+to track your modifications to the database and 
+create checkpoints in case you ever need to roll 
+those modifications back.
+
+Tip: It's always a good idea to start with an empty 
+revision! This allows you to roll all the way back 
+while still holding onto your database. You can create 
+this empty revision with flask db revision -m'Create DB'
+"""
+
+"""
+remember that you will need to use Flask-SQLAlchemy, 
+Flask-Migrate, and SQLAlchemy-Serializer instead of 
+SQLAlchemy and Alembic in your models
+"""
+
 from sqlalchemy_serializer import SerializerMixin
 
 from config import db
--- server/seed.py
+++ server/seed.py
@@ -1,3 +1,14 @@
+"""
+If you want to seed your database, write out your 
+seed.py script and run it to generate some test data. 
+You may want to use Pipenv to install Faker 
+to save you some time.
+"""
+
+"""
+Don't forget to set an app context in your seed file!!!
+"""
+
 #!/usr/bin/env python3
 
 # Standard library imports
@@ -10,7 +21,7 @@ from faker import Faker
 from app import app
 from models import db
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
