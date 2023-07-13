3bb4abc2b07df4c24dc9490c535aa268b0d13bb1
Jay Lim <jlimoman@gmail.com>
2023-07-13 13:05:55 -0600

    complete initial customer model

--- server/models.py
+++ server/models.py
@@ -22,4 +22,37 @@ from sqlalchemy_serializer import SerializerMixin
 
 from config import db
 
+
 # Models go here!
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = "customers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.VARCHARD, nullable=False, unique=True)
+    address = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, unique=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # relationships
+
+    # validations
+
+    # other methods
+    def __repr__(self):
+        return (
+            f"<Customer ID: #{self.id}\n"
+            + f"Name: {self.name}"
+            + f"Email: {self.email}"
+            + f"Address: {self.address}"
+            + f"Password: {self.password}"
+        )
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "": self.id,
+            "id": self.id,
+        }
