f61fe86638013a65f0c4e312ee2379c002b1b794
Jay Lim <jlimoman@gmail.com>
2023-07-14 17:59:51 -0600

    place Login Form lock icon

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Login.css';
-import { Grid, Paper, Avatar } from '@mui/material';
+import { Grid, Paper, Avatar, TextField } from '@mui/material';
+import LockPersonIcon from '@mui/icons-material/LockPerson';
 
 const Login = () => {
-  const paperStyle={padding: 20, height:'70vh', width: 280, margin: '20px auto'}
+  
+  const paperStyle={padding: 20, height:'70vh', width: 280, margin: '20px auto'};
+  const avatarStyle={backgroundColor:'#1bbd7e'}; 
 
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
-        Log In
+        <Grid align='center'>
+          <Avatar style={avatarStyle}>
+            <LockPersonIcon />
+          </Avatar>
+        </Grid>
       </Paper>
     </Grid>
   );
