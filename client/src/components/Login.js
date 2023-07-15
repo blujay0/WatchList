84766b5dc33e68fc24203847bf5b8903cdee8f3c
Jay Lim <jlimoman@gmail.com>
2023-07-14 20:08:17 -0600

    remove avatarStyle const

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import './Login.css';
 import { Grid, Paper, Avatar, TextField, Button, IconButton } from '@mui/material';
-import { LockPersonIcon, FingerprintIcon } from '@mui/icons-material';
+import { LockPerson, Fingerprint } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 const Login = () => {
   
   const paperStyle = {backgroundColor: '#ADD8E6', padding: 20, height:'50vh', width: 300, margin: '20px auto'};
-  const avatarStyle = {backgroundColor:'#1bbd7e'};
-  const buttonStyle = {margin: '8px 0', borderRadius: '10px'};
+  const buttonStyle = {margin: '8px 0', borderRadius: '10px', backgroundColor: '#4682BF', padding: 0};
   const textFieldStyle = {backgroundColor: '#FFFAFA', margin: '8px 0'};
 
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
         <Grid align='center'>
-          <Avatar style={avatarStyle}>
-            <LockPersonIcon />
+          <Avatar>
+            <LockPerson fontSize="medium"/>
           </Avatar>
           <h2><b>LOG IN</b></h2>
         </Grid>
         <TextField label='Email' placeholder='Enter email' style={textFieldStyle} fullWidth required/>
         <TextField label='Password' placeholder='Enter password' style={textFieldStyle} type="password" fullWidth required/>
-        <IconButton aria-label="fingerprint" color="success" type='submit' variant='contained' style={buttonStyle} color='primary' fullWidth>
-          Continue
-        </IconButton>
+        <Button type='submit' style={buttonStyle} fullWidth>
+          <Fingerprint fontSize="large" color='success'/>&nbsp;<h2>Continue</h2>
+        </Button>
         Need an Account? <Link to="/signup">Sign Up</Link>
       </Paper>
     </Grid>
