94f3026c2fa49f7ac5abd970c79c560033808fbd
Jay Lim <jlimoman@gmail.com>
2023-07-14 18:26:32 -0600

    add button style

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,8 +5,9 @@ import LockPersonIcon from '@mui/icons-material/LockPerson';
 
 const Login = () => {
   
-  const paperStyle={padding: 20, height:'70vh', width: 280, margin: '20px auto'};
-  const avatarStyle={backgroundColor:'#1bbd7e'}; 
+  const paperStyle = {padding: 20, height:'70vh', width: 280, margin: '20px auto'};
+  const avatarStyle = {backgroundColor:'#1bbd7e'};
+  const buttonStyle = {margin: '8px 0'}
 
   return (
     <Grid>
@@ -19,8 +20,8 @@ const Login = () => {
         </Grid>
         <TextField label='Email' placeholder='Enter email' fullWidth required/>
         <TextField label='Password' placeholder='Enter password' type="password" fullWidth required/>
-        <Button type='submit' variant='contained' color='primary' fullWidth>Continue</Button>
-        <Link>Link</Link>
+        <Button type='submit' variant='contained' style={buttonStyle} color='primary' fullWidth>Continue</Button>
+        Need an Account? <Link href="">Sign Up</Link>
       </Paper>
     </Grid>
   );
