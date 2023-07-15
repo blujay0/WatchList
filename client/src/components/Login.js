4213cea5d46536e0d603f15fbe029bbcffedf4e5
Jay Lim <jlimoman@gmail.com>
2023-07-14 20:24:33 -0600

    create lockPersonStyle constant and implement in <LockPerson />

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,17 +5,18 @@ import { LockPerson, Fingerprint } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 const Login = () => {
   
-  const paperStyle = {backgroundColor: '#ADD8E6', padding: 20, height:'50vh', width: 300, margin: '20px auto'};
-  const buttonStyle = {margin: '8px 0', height:'5vh', borderRadius: '10px', backgroundColor: '#4682BF', padding: 0};
-  const textFieldStyle = {backgroundColor: '#FFFAFA', margin: '8px 0'};
-  const avatarStyle = { height: '70px', width: '70px', color: '#556B2F' }
+  const paperStyle = { backgroundColor: '#ADD8E6', padding: 20, height:'50vh', width: 300, margin: '20px auto' };
+  const buttonStyle = { margin: '8px 0', height:'5vh', borderRadius: '10px', backgroundColor: '#4682BF', padding: 0 };
+  const textFieldStyle = { backgroundColor: '#FFFAFA', margin: '8px 0' };
+  const avatarStyle = { height: '70px', width: '70px', color: '#556B2F' };
+  const lockPersonStyle = { fontSize: '2em', color: 'white' };
 
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
         <Grid align='center'>
           <Avatar sx={avatarStyle}>
-            <LockPerson fontSize="large" />
+            <LockPerson sx={lockPersonStyle} />
           </Avatar>
           <h1><b>LOG IN</b></h1>
         </Grid>
