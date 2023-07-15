12c35428160f31bce57b54302129357a27aea98e
Jay Lim <jlimoman@gmail.com>
2023-07-15 00:00:17 -0600

    initial Footer with all links and elements showing

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,16 +1,34 @@
 import "./Footer.css"
 import { Link } from "react-router-dom" // Link component replaces the anchor tags
-import { TwitterIcon, FacebookIcon, InstagramIcon, MailOutlineIcon } from '@mui/icons-material'
+import { Twitter, Facebook, Instagram, MailOutline } from '@mui/icons-material'
 
 function Footer() {
-  return <nav className="footer">
-    <Link to="/" className="site-title">
-      WatchList
-    </Link>
-    <Link to="/about">
-      About
-    </Link>
-  </nav>
+  return (
+    <footer className="footer">
+      <Link to="/" className="site-title">
+        WatchList
+      </Link>
+      <Link to="/about">
+        About
+      </Link>
+      <a href='https://twitter.com' target="_blank" rel='noreferrer'>
+        <Twitter className='socials-icon' sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
+      </a>
+      <a href='https://facebook.com' target="_blank" rel='noreferrer'>
+        <Facebook className='socials-icon' sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
+      </a>
+      <a href='https://instagram.com' target="_blank" rel='noreferrer'>
+        <Instagram className='socials-icon' sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
+      </a>
+      <a href='https://gmail.com/' target="_blank" rel='noreferrer'>
+        <MailOutline className='socials-icon' sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
+      </a>
+      <p>Guitar Trader, Inc. &#8482;</p> 
+      {/* <a href='#root'>
+        <button className="back-button" style={{marginLeft: '10px'}}> Back to Top</button>
+      </a>       */}
+    </footer>
+  )
 }
 
 export default Footer
