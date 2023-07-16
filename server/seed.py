4a4a100ded4e13eabd03d7c82f8c991475a642c4
Jay Lim <jlimoman@gmail.com>
2023-07-16 04:51:03 -0600

    first product seed defined and db.session commands added for Product

--- server/seed.py
+++ server/seed.py
@@ -23,6 +23,45 @@ from models import db, Customer, Order, CartItem, OrderDetail, Product
 
 fake = Faker()
 
+with app.app_context():  # necessary to be in context of your application
+    Customer.query.delete()
+    Order.query.delete()
+    CartItem.query.delete()
+    OrderDetail.query.delete()
+    Product.query.delete()
+
+    p1 = Product(
+        maker="Timex",
+        model="manual",
+        product_name="Marlin",
+        product_price=200,
+        inventory=fake.random_int(1, 10000),
+        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+    )
+
+    p2 = Product()
+
+    p3 = Product()
+
+    p4 = Product()
+
+    p5 = Product()
+
+    p6 = Product()
+
+    p7 = Product()
+
+    p8 = Product()
+
+    p9 = Product()
+
+    p10 = Product()
+
+    products = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
+
+    db.session.add_all(products)
+    db.session.commit()
 
 if __name__ == "__main__":
     fake = Faker()
