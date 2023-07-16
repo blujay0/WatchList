45df7bcb63bf44f3980eda5e21c098c8436b1cb0
Jay Lim <jlimoman@gmail.com>
2023-07-16 05:15:54 -0600

    product 3 and 4 seed def done

--- server/seed.py
+++ server/seed.py
@@ -51,23 +51,23 @@ with app.app_context():  # necessary to be in context of your application
     )
 
     p3 = Product(
-        maker="Timex",
+        maker="Orient",
         model="manual",
-        product_name="Marlin",
-        product_price=200,
+        product_name="Monarch",
+        product_price=450,
         inventory=fake.random_int(1, 10000),
-        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
-        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+        product_description="While often difficult to find, with limited distribution at least in the United States, the Orient Monarch is one of the most traditional and charming manual-winding watches on the market from a more attainable point of view. Complete with printed Arabic numerals that look like they’ve been neatly applied with a calligrapher’s ink pen, the Monarch is a pure traditional watch design updated with a modern hacking caliber from Orient with 40 hours of power reserve as well as 50 meters of capable water resistance for additional peace of mind. Positioned at 40mm in diameter and only 45mm in length, the Monarch should wear well on all but the smallest or largest wrists, providing a dressier option for anyone looking for a straightforward and classic watch that looks significantly more expensive than it is.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Orient-2.jpg?v=1649173137/200",
     )
 
     p4 = Product(
-        maker="Timex",
+        maker="Citizen",
         model="automatic",
-        product_name="Marlin",
-        product_price=200,
+        product_name="NY0040-09E",
+        product_price=400,
         inventory=fake.random_int(1, 10000),
-        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
-        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+        product_description="The broad collection of 42mm Promaster automatic dive watches, including this NY0040-09E, has long served as a capable alternative to Seiko, providing a hacking and hand-winding Miyota caliber for essentially the same price as the SKX with its simpler 7S26, and also serving up dozens of dial and bezel color combinations to suit virtually any taste. It’s a relatively basic ISO-certified dive watch, yes, but it’s also a legitimate undersea tool proven on the wrists of countless military, commercial, and recreational divers over the years. And with the SKX’s discontinuation in 2019 and the rapidly ascending prices that came along with it, the future of the NY0040 as an excellent alternative looks better than ever.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Citizen-Diver-1.jpg?v=1649189137/200",
     )
 
     p5 = Product(
