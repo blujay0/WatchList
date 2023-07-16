f7135d7c06c0e53f90cea20e0301e7fa1ca18eef
Jay Lim <jlimoman@gmail.com>
2023-07-16 04:57:43 -0600

    second product seed defined and db.session commands added for Product

--- server/seed.py
+++ server/seed.py
@@ -40,32 +40,104 @@ with app.app_context():  # necessary to be in context of your application
         image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
     )
 
-    p2 = Product()
+    p2 = Product(
+        maker="Hamilton",
+        model="manual",
+        product_name="Khaki Field",
+        product_price=500,
+        inventory=fake.random_int(1, 10000),
+        product_description="For under $500, the Hamilton Khaki Field Mechanical is perhaps the best Swiss mechanical watch on the market, full stop. It contains a modified Swiss ETA 2801 caliber that Hamilton calls the H-50, which drops the beat frequency from the standard 28,000 VPH to 21,600 VPH to stretch the power reserve out to an impressive 80 hours. And the longer power reserve here is especially impactful with this being a hand-wound watch. Beyond the movement, the Khaki Field Mechanical leans into Hamilton’s impressive history of supplying field watches to the U.S. and other Allied military organizations in World War II, with this basic dial design dating back decades. In the modern world, the Khaki Field Mechanical (you can learn more about it here) is an impressive value with a lot to offer for both military missions as well as life’s everyday operations.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Hamilton-Khaki-Field-Manual-2.jpg?v=1649188187/200",
+    )
 
-    p3 = Product()
+    p3 = Product(
+        maker="Timex",
+        model="manual",
+        product_name="Marlin",
+        product_price=200,
+        inventory=fake.random_int(1, 10000),
+        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+    )
 
-    p4 = Product()
+    p4 = Product(
+        maker="Timex",
+        model="manual",
+        product_name="Marlin",
+        product_price=200,
+        inventory=fake.random_int(1, 10000),
+        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+    )
 
-    p5 = Product()
+    p5 = Product(
+        maker="Timex",
+        model="manual",
+        product_name="Marlin",
+        product_price=200,
+        inventory=fake.random_int(1, 10000),
+        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+    )
 
-    p6 = Product()
+    p6 = Product(
+        maker="Timex",
+        model="manual",
+        product_name="Marlin",
+        product_price=200,
+        inventory=fake.random_int(1, 10000),
+        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+    )
 
-    p7 = Product()
+    p7 = Product(
+        maker="Timex",
+        model="manual",
+        product_name="Marlin",
+        product_price=200,
+        inventory=fake.random_int(1, 10000),
+        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+    )
 
-    p8 = Product()
+    p8 = Product(
+        maker="Timex",
+        model="manual",
+        product_name="Marlin",
+        product_price=200,
+        inventory=fake.random_int(1, 10000),
+        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+    )
 
-    p9 = Product()
+    p9 = Product(
+        maker="Timex",
+        model="manual",
+        product_name="Marlin",
+        product_price=200,
+        inventory=fake.random_int(1, 10000),
+        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+    )
 
-    p10 = Product()
+    p10 = Product(
+        maker="Timex",
+        model="manual",
+        product_name="Marlin",
+        product_price=200,
+        inventory=fake.random_int(1, 10000),
+        product_description="Housed within the safety of a diminutive 34mm wide stainless steel case that only measures 41mm in length, the modern Marlin still feels very much of the midcentury, presenting an impressively inexpensive take on the Don Draper design language that seems to be permeating our vintage-obsessed cultural marketplace. Equipped with a heavily-domed acrylic crystal, a range of intriguing dial finishes and colors, and a reliable but simple Seagull caliber from China, the Timex Marlin is the definitive hand-winding dress watch for its staggeringly-low $200 retail price.",
+        image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Timex-marlin-handwound-2.jpg?v=1649256945/200",
+    )
 
     products = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
 
     db.session.add_all(products)
     db.session.commit()
-    
-    c1 = Customer(
-        name= , email= , address= , password= , create_at= , updated_at= ,
-    )
+
+    # c1 = Customer(
+    #     name= , email= , address= , password= , create_at= , updated_at= ,
+    # )
 
 if __name__ == "__main__":
     fake = Faker()
