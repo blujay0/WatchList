36c8c050db29bb6c52bc5e6583ada1b6ae4067ea
Jay Lim <jlimoman@gmail.com>
2023-07-16 05:54:09 -0600

    install ipdb and faker

--- server/seed.py
+++ server/seed.py
@@ -230,7 +230,28 @@ with app.app_context():  # necessary to be in context of your application
         image="https://cdn.shopify.com/s/files/1/0278/9723/3501/files/Bulova-Sutton-2.jpg?v=1649252209/200",
     )
 
-    products = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
+    products = [
+        p1,
+        p2,
+        p3,
+        p4,
+        p5,
+        p6,
+        p7,
+        p8,
+        p9,
+        p10,
+        p11,
+        p12,
+        p13,
+        p14,
+        p15,
+        p16,
+        p17,
+        p18,
+        p19,
+        p20,
+    ]
 
     db.session.add_all(products)
     db.session.commit()
