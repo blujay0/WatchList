ef66e649028873b3896d2c97869d64c30164e226
Jay Lim <jlimoman@gmail.com>
2023-07-24 13:43:11 -0600

    useContext theme applied to SignUp page

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,7 +3,8 @@ import './SignUp.css';
 import { Grid, Paper, Avatar, TextField, Button, Box, IconButton, Divider } from '@mui/material';
 import { LockPerson, Fingerprint, Person } from '@mui/icons-material';
 import { Link, useHistory } from 'react-router-dom';
-
+import { ThemeContext } from './App.js'
+import { useTheme } from './ThemeProvider'
 
 const SignUp = () => {
   const [name, setName] = useState();
@@ -19,6 +20,11 @@ const SignUp = () => {
   const avatarStyle = { height: '70px', width: '70px', bgcolor: '#273248', fontSize: 30 };
   const personStyle = { fontSize: '2em', color: 'white' };
   const fingerprintStyle = { fontSize: '40px', color: 'white' };
+
+  const darkTheme = useTheme();
+  const themeStyles = {
+    backgroundColor: darkTheme ? '#008B8B' : '#FFFFFF'
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -47,34 +53,37 @@ const SignUp = () => {
   }
 
   return (
-    <Grid>
-      <Paper elevation={0} style={paperStyle}>
-        <Grid align='center'>
-          <Avatar sx={avatarStyle}>
-            <Person sx={personStyle} />
-          </Avatar>
-          <h1><b>SIGN UP</b></h1>
-        </Grid>
-        <form onSubmit={handleSubmit}>
-          <TextField onChange={e => setName(e.target.value)} value={name} label='name' placeholder='First and last name' style={textFieldStyle} fullWidth required/>
-          <TextField onChange={e => setEmail(e.target.value)} label='email' placeholder='Enter email' style={textFieldStyle} fullWidth required/>
-          <TextField onChange={e => setAddress(e.target.value)} label='address' placeholder='Enter your address' style={textFieldStyle} fullWidth required/>        
-          <TextField onChange={e => setPassword(e.target.value)} label='password' placeholder='Enter password' style={textFieldStyle} type="password" fullWidth required/>
-          <Box textAlign="center">
-            {errorMessage && <div style={{color:"red"}}>{errorMessage}</div>}
-            <Button type='submit' style={buttonStyle} fullWidth>
-                <Fingerprint sx={fingerprintStyle}/>&nbsp;<p style={{color: "white", fontSize: '20px'}}><b>Register</b></p>
-            </Button>          
-          </Box>
-        </form>
-        <br/>
-        <br/>
-        <Divider>OR</Divider>
-        <br/>
-        <br/>     
-        <center style={{fontSize: '20px'}}>Already Have an Account? <Link to="/login">Log In</Link></center>
-      </Paper>
-    </Grid>
+    <div style={themeStyles}>
+      <Grid>
+        <Paper elevation={0} style={paperStyle}>
+          <Grid align='center'>
+            <Avatar sx={avatarStyle}>
+              <Person sx={personStyle} />
+            </Avatar>
+            <h1><b>SIGN UP</b></h1>
+          </Grid>
+          <form onSubmit={handleSubmit}>
+            <TextField onChange={e => setName(e.target.value)} value={name} label='name' placeholder='First and last name' style={textFieldStyle} fullWidth required/>
+            <TextField onChange={e => setEmail(e.target.value)} label='email' placeholder='Enter email' style={textFieldStyle} fullWidth required/>
+            <TextField onChange={e => setAddress(e.target.value)} label='address' placeholder='Enter your address' style={textFieldStyle} fullWidth required/>        
+            <TextField onChange={e => setPassword(e.target.value)} label='password' placeholder='Enter password' style={textFieldStyle} type="password" fullWidth required/>
+            <Box textAlign="center">
+              {errorMessage && <div style={{color:"red"}}>{errorMessage}</div>}
+              <Button type='submit' style={buttonStyle} fullWidth>
+                  <Fingerprint sx={fingerprintStyle}/>&nbsp;<p style={{color: "white", fontSize: '20px'}}><b>Register</b></p>
+              </Button>          
+            </Box>
+          </form>
+          <br/>
+          <br/>
+          <Divider>OR</Divider>
+          <br/>
+          <br/>     
+          <center style={{fontSize: '20px'}}>Already Have an Account? <Link to="/login">Log In</Link></center>
+        </Paper>
+      </Grid>
+    </div>
+
   );
 }
 
