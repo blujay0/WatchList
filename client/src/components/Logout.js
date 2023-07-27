eda57c8e115a3e687f29587e691b7fc4ec420822
Jay Lim <jlimoman@gmail.com>
2023-07-27 15:49:27 -0600

    restyle Logout button so it is easier to see for the user

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -2,10 +2,16 @@ import React, { useContext } from 'react'
 import { useHistory } from 'react-router-dom';
 import { ThemeContext } from './App.js'
 import { SuccessContext } from '../context/SuccessContext.js';
+import { Button } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
 
-const Logout = ({updateCustomer, setCustomer}) => { // updateCustomer is not defined yet, but remember you will implement useContext
+const Logout = ({updateCustomer, setCustomer}) => { 
   const history = useHistory();
   const { success, setSuccess } = useContext(SuccessContext)
+
+  const logoutStyle = { fontSize: '40px', color: 'black' };
+  const buttonStyle = { margin: '8px 0', height:'5vh', width: 200, borderRadius: '30px', backgroundColor: 'yellow', padding: 0, position: "absolute", top: "25%", left:"50%", transform: "translate(-50%, -50%)" };
+
   const handleClick = async () => {
       fetch("/logout", {
           method: "POST",
@@ -21,7 +27,11 @@ const Logout = ({updateCustomer, setCustomer}) => { // updateCustomer is not def
   }
 
 return (
-  <button style={{position: "absolute", top: "25%", left:"50%", transform: "translate(-50%, -50%)", background: "yellow"}} onClick={handleClick}>Logout</button>
+  // <button style={{position: "absolute", top: "25%", left:"50%", transform: "translate(-50%, -50%)", background: "yellow"}} onClick={handleClick}>Logout</button>
+  <Button onClick={handleClick} style={buttonStyle}>
+    <LogoutIcon sx={logoutStyle}/>&nbsp;<p style={{color: "black", fontSize: '20px'}}><b>Logout</b></p>
+  </Button>  
+  
 )
 }
 
