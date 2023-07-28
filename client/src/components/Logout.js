98780fe5d703ba5e498b104c0fc574e846687295
Jay Lim <jlimoman@gmail.com>
2023-07-28 00:20:47 -0600

    small update

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { useHistory } from 'react-router-dom';
 import { ThemeContext } from './App.js';
 import { useTheme } from './ThemeProvider';
 import { SuccessContext } from '../context/SuccessContext.js';
 import { Button } from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
+import Confetti from 'react-confetti'
 
 const Logout = ({updateCustomer, setCustomer}) => { 
   const history = useHistory();
@@ -17,6 +18,37 @@ const Logout = ({updateCustomer, setCustomer}) => {
   const themeStyles = {
     backgroundColor: darkTheme ? '#154F49' : '#FFFFFF'
   }
+
+//   const [windowSize, setWindowSize] = useState({
+//     width: undefined,
+//     height: undefined,
+//   })
+
+//   const handleWindowSize = () => {
+//     setWindowSize({
+//       width:window.innerWidth,
+//       height: window.innerHeight,
+//     });
+//   }
+
+//   useEffect(() => {
+//     window.onresize = () => handleWindowSize();
+//   }, [])
+
+//   const handleClick = async () => {
+    
+//     fetch("/logout", {
+//         method: "POST",
+//     })
+//     .then(resp => {
+//       if(resp.ok) {
+//         setCustomer(null);
+//         setSuccess('Logged Out!')
+//         history.push('/login')
+//       }        
+//     })
+//     // setCustomer(null);
+// }
 
   const handleClick = async () => {
       fetch("/logout", {
@@ -33,8 +65,10 @@ const Logout = ({updateCustomer, setCustomer}) => {
   }
 
 return (
+  
   // <button style={{position: "absolute", top: "25%", left:"50%", transform: "translate(-50%, -50%)", background: "yellow"}} onClick={handleClick}>Logout</button>
   <div style={themeStyles}>
+    {/* <Confetti width={windowSize.width} height={windowSize.height} /> */}
     <Button onClick={handleClick} style={buttonStyle}>
       <LogoutIcon sx={logoutStyle}/>&nbsp;<p style={{color: "black", fontSize: '20px'}}><b>Logout</b></p>
     </Button>
